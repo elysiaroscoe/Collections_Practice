483134d8d0e90c38593995aca2eaf42c4d54f27b
Roscoe <elysiaroscoe@gmail.com>
2021-11-30 14:59:12 -0800

    commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Collections_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numarray = new int[] {0,1,2,3,4,5,6,7,8,9};
+            IntArray(numarray);
+        }
+
+
+        public static void IntArray(int[] numbers)
+        {
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+
+    }
+}
